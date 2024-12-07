59f46054205045a1c60850fc3b6e3ff273174bcc
yemparalamanasvi <yemparalamanasvi@gmail.com>
2024-12-07 08:27:19 +0530

    service and controller class codes are pushed

---
+++ src/main/java/com/klef/jfsd/exam/SpringbootresttemplateApplication.java
@@ -0,0 +1,14 @@
+package com.klef.jfsd.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootresttemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootresttemplateApplication.class, args);
+		System.out.println("SPRING BOOT Rest template");
+	}
+
+}
